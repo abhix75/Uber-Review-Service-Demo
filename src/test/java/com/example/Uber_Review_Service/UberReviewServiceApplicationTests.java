b0894a03f4dab44d458e6c58900e7167a55a8d02
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-06-16 23:31:14 +0530

    Added the Basic Spring data JPA configuration

---
+++ src/test/java/com/example/Uber_Review_Service/UberReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Uber_Review_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
