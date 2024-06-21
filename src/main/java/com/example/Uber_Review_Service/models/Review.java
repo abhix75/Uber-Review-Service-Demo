88667fdd24de030c196f5f06aaa71a1944b01b25
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-06-21 23:03:47 +0530

    Added some columns to thye table

--- src/main/java/com/example/Uber_Review_Service/models/Review.java
+++ src/main/java/com/example/Uber_Review_Service/models/Review.java
@@ -1,11 +1,28 @@
 package com.example.Uber_Review_Service.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
 @Entity
+@Table(name = "BookinReview")
 public class Review {
-
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updatedAt;
 }
